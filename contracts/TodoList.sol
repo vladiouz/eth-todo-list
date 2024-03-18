5d472c03345599a1e09143fc0213217a00b89a1f
Petru-Vlad Ionescu <92223782+vladiouz@users.noreply.github.com>
2024-03-18 14:56:32 +0200

    Add functionality for toggling tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -10,12 +10,32 @@ contract TodoList {
     }
 
     constructor() public {
-        createTask("hi mom<3");
+        createTask("buy milk");
     }
 
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
+
     mapping(uint => Task) public tasks;
+    
     function createTask(string memory _content) public {
         taskCount++;
         tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
+
+    event TaskCompleted(
+        uint id,
+        bool completed
+    );
+
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
     }
 }
