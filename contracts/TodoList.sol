55f1bd447e0b8e01955487c342928e6929dda928
Petru-Vlad Ionescu <92223782+vladiouz@users.noreply.github.com>
2024-03-18 13:49:50 +0200

    Add client side and tests

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,5 +2,20 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public taskCount = 0;
-    
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    constructor() public {
+        createTask("hi mom<3");
+    }
+
+    mapping(uint => Task) public tasks;
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
